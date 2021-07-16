7543297bf08ef01c1813d46b3b22f01798ddf588
Rinaldi Pratama Putra <rinaldiprtma@gmail.com>
2021-07-16 08:57:41 +0700

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import TitleText from './TitleText';
+import colors from '../constants/colors';
+
+const Header = props => {
+    return (
+        <View style={styles.header}>
+            <TitleText style={styles.headerTitle}>{props.title}</TitleText>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 90,
+        paddingTop: 36,
+        backgroundColor: colors.primary,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headerTitle: {
+        color: 'white',
+        fontSize: 20,
+        fontFamily: 'open-sans-bold'
+    }
+});
+
+export default Header;
---
+++ constants/colors.js
@@ -0,0 +1,4 @@
+export default {
+    primary: '#00B0FF',
+    secondary: '#868E96'
+};
